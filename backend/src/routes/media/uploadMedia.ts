ecda108ee8a2e774e8d2654e1697aad47451ea5e
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2024-11-20 12:26:58 +0200

    Add JSON schema for `uploadMedia.ts` route

--- backend/src/routes/media/uploadMedia.ts
+++ backend/src/routes/media/uploadMedia.ts
@@ -1,6 +1,52 @@
-import {FastifyPluginAsync} from 'fastify'
+import { FastifyPluginAsync } from 'fastify'
 
-const uploadMedia: FastifyPluginAsync = async (fastify, opts): Promise < void > => {
+// TODO: Move this into a separate file
+const uploadMediaSchema = {
+  "tags": ["media"],
+  "summary": "Upload a media file",
+  "body": {
+    "type": "object",
+    "properties": {
+      "file": {
+        "type": "string",
+        "format": "binary",
+        "description": "The file to upload"
+      }
+    },
+    "required": ["file"]
+  },
+  "response": {
+    "200": {
+      "description": "File uploaded successfully",
+      "type": "object",
+      "properties": {
+        "message": {
+          "type": "string"
+        }
+      }
+    },
+    "400": {
+      "description": "Invalid file type or no file uploaded",
+      "type": "object",
+      "properties": {
+        "message": {
+          "type": "string"
+        }
+      }
+    },
+    "500": {
+      "description": "Error uploading file",
+      "type": "object",
+      "properties": {
+        "message": {
+          "type": "string"
+        }
+      }
+    }
+  }
+}
+
+const uploadMedia: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   const { BUCKET_NAME } = process.env as { BUCKET_NAME: string };
 
   const uploadToMinIO = async (bucketName: string, objectName: string, buffer: Buffer) => {
@@ -14,7 +60,7 @@ const uploadMedia: FastifyPluginAsync = async (fastify, opts): Promise < void > 
       }
   };
 
-  fastify.post('/upload', async (request, reply) => {
+  fastify.post('/upload', {schema: uploadMediaSchema}, async (request, reply) => {
       const data = await request.file();
 
       if (!data) {
@@ -27,7 +73,7 @@ const uploadMedia: FastifyPluginAsync = async (fastify, opts): Promise < void > 
       await ensureBucketExists(BUCKET_NAME);
       await uploadToMinIO(BUCKET_NAME, fileName, fileBuffer);
 
-      return reply.send({message: 'File uploaded successfully!'});
+      return reply.send({ message: 'File uploaded successfully!' });
   });
 }
 
