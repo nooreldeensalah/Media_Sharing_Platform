f85f0e82d2f0927a5b2744df74c90b88704c36eb
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2024-11-20 12:21:34 +0200

    Add JSON schema for `getMedia.ts` route

--- backend/src/routes/media/getMedia.ts
+++ backend/src/routes/media/getMedia.ts
@@ -1,9 +1,54 @@
 import { FastifyPluginAsync } from 'fastify'
 
+// TODO: Move this into a separate file
+const getMediaSchema = {
+  "tags": ["media"],
+  "summary": "Get a presigned URL for a media file",
+  "params": {
+    "type": "object",
+    "properties": {
+      "fileName": {
+        "type": "string",
+        "description": "The name of the file to retrieve"
+      }
+    },
+    "required": ["fileName"]
+  },
+  "response": {
+    "200": {
+      "description": "Presigned URL for the media file",
+      "type": "object",
+      "properties": {
+        "url": {
+          "type": "string"
+        }
+      }
+    },
+    "404": {
+      "description": "File not found",
+      "type": "object",
+      "properties": {
+        "message": {
+          "type": "string"
+        }
+      }
+    },
+    "500": {
+      "description": "Error retrieving file",
+      "type": "object",
+      "properties": {
+        "message": {
+          "type": "string"
+        }
+      }
+    }
+  }
+}
+
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   const { BUCKET_NAME } = process.env as { BUCKET_NAME: string };
 
-  fastify.get('/:fileName', async (request, reply) => {
+  fastify.get('/:fileName', {  schema: getMediaSchema }, async (request, reply) => {
     const { fileName } = request.params as { fileName: string };
 
     try {
