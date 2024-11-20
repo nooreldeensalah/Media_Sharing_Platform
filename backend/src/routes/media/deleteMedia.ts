cac3404ad71126980db9529a975f76722826bdd1
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2024-11-20 12:28:54 +0200

    Add JSON schema to `deleteMedia.ts` route

--- backend/src/routes/media/deleteMedia.ts
+++ backend/src/routes/media/deleteMedia.ts
@@ -1,9 +1,54 @@
 import { FastifyPluginAsync } from 'fastify'
 
+// TODO: Add this to a separate file
+const deleteMediaSchema = {
+  "tags": ["media"],
+  "summary": "Delete a media file",
+  "params": {
+    "type": "object",
+    "properties": {
+      "fileName": {
+        "type": "string",
+        "description": "The name of the file to delete"
+      }
+    },
+    "required": ["fileName"]
+  },
+  "response": {
+    "200": {
+      "description": "File deleted successfully",
+      "type": "object",
+      "properties": {
+        "message": {
+          "type": "string"
+        }
+      }
+    },
+    "404": {
+      "description": "File not found",
+      "type": "object",
+      "properties": {
+        "message": {
+          "type": "string"
+        }
+      }
+    },
+    "500": {
+      "description": "Error deleting file",
+      "type": "object",
+      "properties": {
+        "message": {
+          "type": "string"
+        }
+      }
+    }
+  }
+}
+
 const deleteMedia: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   const { BUCKET_NAME } = process.env as { BUCKET_NAME: string };
 
-  fastify.delete('/:fileName', async (req, reply) => {
+  fastify.delete('/:fileName', { schema: deleteMediaSchema }, async (req, reply) => {
     const { fileName } = req.params as { fileName: string };
 
     try {
