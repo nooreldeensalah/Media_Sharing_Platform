f131c80a6cc12d9c79b836fa6ba38add19a3d5df
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2024-11-20 12:23:42 +0200

    Add JSON schema for `getAllMedia.ts` route

--- backend/src/routes/media/getAllMedia.ts
+++ backend/src/routes/media/getAllMedia.ts
@@ -1,9 +1,41 @@
 import { FastifyPluginAsync } from 'fastify'
 
+// TODO: Move this into a separate file
+const getAllMediaSchema = {
+    "tags": ["media"],
+    "summary": "Get all media files",
+    "response": {
+      "200": {
+        "description": "List of media files",
+        "type": "array",
+        "items": {
+          "type": "object",
+          "properties": {
+            "name": {
+              "type": "string"
+            },
+            "url": {
+              "type": "string"
+            }
+          }
+        }
+      },
+      "500": {
+        "description": "Error fetching media files",
+        "type": "object",
+        "properties": {
+          "message": {
+            "type": "string"
+          }
+        }
+      }
+    }
+}
+
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   const { BUCKET_NAME } = process.env as { BUCKET_NAME: string };
 
-  fastify.get('/', async (req, reply) => {
+  fastify.get('/', {schema: getAllMediaSchema}, async (req, reply) => {
     try {
         const objectsStream = fastify.minio.listObjects(BUCKET_NAME, '', true);
         const files: { name: string; url: string }[] = [];
