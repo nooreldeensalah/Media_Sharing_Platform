948868bcc84ebc85acfa21fcda5c11e7475cdb79
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2024-11-21 07:18:34 +0200

    Modify `getMedia.ts` route to retrieve media file from database

--- backend/src/routes/media/getMedia.ts
+++ backend/src/routes/media/getMedia.ts
@@ -19,7 +19,23 @@ const getMediaSchema = {
       "description": "Presigned URL for the media file",
       "type": "object",
       "properties": {
+        "id": {
+          "type": "integer"
+        },
+        "file_name": {
+          "type": "string"
+        },
+        "likes": {
+          "type": "integer"
+        },
         "url": {
+          "type": "string"
+        },
+        "created_at": {
+          "type": "string",
+          "format": "date-time"
+        },
+        "mimetype": {
           "type": "string"
         }
       }
@@ -46,18 +62,19 @@ const getMediaSchema = {
 }
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  const { BUCKET_NAME } = process.env as { BUCKET_NAME: string };
-
-  fastify.get('/:fileName', {  schema: getMediaSchema }, async (request, reply) => {
+  fastify.get('/:fileName', { schema: getMediaSchema }, async (request, reply) => {
     const { fileName } = request.params as { fileName: string };
 
     try {
-        const url = await fastify.minio.presignedGetObject(BUCKET_NAME, fileName);
-        reply.send({ url });
+        const { rows } = await fastify.pg.query('SELECT * FROM media WHERE file_name = $1', [fileName]);
+        if (rows.length === 0) {
+            return reply.notFound('File not found in database');
+        }
+        return reply.send(rows[0]);
     } catch (err) {
-        reply.notFound('File not found');
+            return reply.internalServerError('Error retrieving file');
     }
-});
+  });
 }
 
 export default root;
