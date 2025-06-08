2bbf159e6776738cdfe7ba8d3ca24c9e4663272b
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2025-06-08 21:48:31 +0300

    Add environment variables validation using `fastify-env` plugin

--- backend/src/plugins/env.ts
+++ backend/src/plugins/env.ts
@@ -5,8 +5,19 @@ export default fp<FastifyEnvOptions>(async (fastify) => {
   fastify.register(env, {
     dotenv: true,
     schema: {
-      type: 'object'
+      type: 'object',
+      required: ['JWT_SECRET', 'DATABASE_FILE_NAME', 'BUCKET_NAME'],
+      properties: {
+        JWT_SECRET: { type: 'string', minLength: 32 },
+        DATABASE_FILE_NAME: { type: 'string' },
+        BUCKET_NAME: { type: 'string' },
+        S3_ACCESS_KEY: { type: 'string' },
+        S3_SECRET_KEY: { type: 'string' },
+        S3_ENDPOINT: { type: 'string' },
+        S3_REGION: { type: 'string' },
+        FASTIFY_PORT: { type: 'string', default: '3000' },
+        FASTIFY_ADDRESS: { type: 'string', default: '0.0.0.0' }
+      }
     }
-  }
-  )
+  })
 })
